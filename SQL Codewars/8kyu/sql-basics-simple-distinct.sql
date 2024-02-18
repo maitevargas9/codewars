f6c90531688e71dc5b478e169b223015a1c7c9f4
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2024-02-18 18:33:54 +0100

    Add challenges

---
+++ SQL Codewars/8kyu/sql-basics-simple-distinct.sql
@@ -0,0 +1,6 @@
+-- Description
+-- For this challenge you need to create a simple DISTINCT statement, you want to find all the unique ages.
+-- people table schema: id name age
+-- select table schema: age (distinct)
+
+SELECT distinct(age) AS age FROM people;
--- SQL Codewars/8kyu/sql-grasshopper-select-columns.sql
+++ SQL Codewars/8kyu/sql-grasshopper-select-columns.sql
@@ -1,4 +1,5 @@
 -- Description
 -- Greetings Grasshopper!
 -- Using only SQL, write a query that returns all rows in the custid, custname, and custstate columns from the customers table.
+
 SELECT custid, custname, custstate FROM customers;
