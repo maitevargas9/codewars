6bcd480a614e5cb6fbb50b47d40ac064e8ce322c
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2024-02-20 18:32:29 +0100

    Add challenges

---
+++ Typescript Codewars/8kyu/is-it-a-palindrome.ts
@@ -0,0 +1,12 @@
+/*
+Description
+
+Write a function that checks if a given string (case insensitive) is a palindrome.
+A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar.
+*/
+
+export function isPalindrome(x: string): boolean {
+  const original = x.toLowerCase().replace(/[^A-Za-z0-9]/g, '');
+  const reversed = original.split('').reverse().join('');
+  return original === reversed;
+}
