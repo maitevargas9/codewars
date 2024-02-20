c77254b8aad4cee81365e3b986d6c3fc10d1981e
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2024-02-20 13:28:41 +0100

    Add challenges

---
+++ Typescript Codewars/7kyu/vowel-count.ts
@@ -0,0 +1,14 @@
+/*
+Description
+
+Return the number (count) of vowels in the given string.
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+The input string will only consist of lower case letters and/or spaces.
+*/
+
+export class Kata {
+  static getCount(str: string): number {
+    let m = str.match(/[aeiou]/gi);
+    return m === null ? 0 : m.length;
+  }
+}
