3baf6a858f54b04f08f2416ff800f18920f250b4
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2024-02-20 20:55:23 +0100

    Add challenges

---
+++ Typescript Codewars/8kyu/difference-of-volumes-of-cuboids.ts
@@ -0,0 +1,14 @@
+/*
+Description
+
+In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+Your function will be tested with pre-made examples as well as random ones.
+If you can, try writing it in one line of code.
+*/
+
+export function findDifference(a: [number, number, number], b: [number, number, number]): number {
+  let volumeA = a[0] * a[1] * a[2];
+  let volumeB = b[0] * b[1] * b[2];
+  return Math.max(volumeA, volumeB) - Math.min(volumeA, volumeB);
+}
