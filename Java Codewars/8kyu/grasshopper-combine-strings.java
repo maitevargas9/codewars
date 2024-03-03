3710c079bb3cb023eaab3f9809dfca1660866818
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2024-03-03 18:43:18 +0100

    Add challenges

---
+++ Java Codewars/8kyu/grasshopper-combine-strings.java
@@ -0,0 +1,15 @@
+/*
+Description
+
+Combine strings function
+
+Example
+combineNames('James', 'Stevens')
+returns 'James Stevens'
+*/
+
+public class StringCombiner {
+  public static String combineNames(String first, String last) {
+    return first + " " + last;
+  }
+}
---
+++ SQL Codewars/8kyu/easy-sql-convert-to-hexadecimal.sql
@@ -0,0 +1,6 @@
+-- Description
+-- monsters table schema id name legs arms characteristics
+-- Your task is to turn the numeric columns (arms, legs) into equivalent hexadecimal values.
+-- output table schema legs arms
+
+SELECT to_hex(legs) as legs, to_hex(arms) as arms FROM monsters;
