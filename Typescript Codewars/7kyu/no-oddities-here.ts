bbfa6f3d94ce955541278d7f26e076627dd78841
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2024-03-03 19:20:20 +0100

    Add challenges

---
+++ Typescript Codewars/7kyu/no-oddities-here.ts
@@ -0,0 +1,10 @@
+/*
+Description
+
+Write a small function that returns the values of an array that are not odd.
+All values in the array will be integers. Return the good values in the order they are given.
+*/
+
+export function noOdds(values: number[]): number[] {
+  return values.filter(x => x % 2 === 0);
+}
