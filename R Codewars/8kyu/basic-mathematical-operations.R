61b1a6cd47b40bf722696c995e80d4ce1946ad64
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2024-04-10 19:43:26 +0200

    Add challenges

---
+++ R Codewars/8kyu/basic-mathematical-operations.R
@@ -0,0 +1,13 @@
+# Description
+# Your task is to create a function that does four basic mathematical operations.
+# The function should take three arguments - operation(string/char), value1(number), value2(number).
+# The function should return result of numbers after applying the chosen operation.
+# Examples(Operator, value1, value2) --> output
+# ('+', 4, 7) --> 11
+# ('-', 15, 18) --> -3
+# ('*', 5, 5) --> 25
+# ('/', 49, 7) --> 7
+
+basic_op <- function(operator, value1, value2) {
+  return(eval(parse(text = paste(value1, operator, value2))))
+}
