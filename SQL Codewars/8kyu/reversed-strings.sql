15aecbe34e3fccd40372ecb32b8295c82326e1aa
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2024-05-27 19:34:21 +0200

    Add challenges

---
+++ SQL Codewars/8kyu/reversed-strings.sql
@@ -0,0 +1,9 @@
+-- Description
+-- Complete the solution so that it reverses the string passed into it.
+-- 'world'  =>  'dlrow'
+-- 'word'   =>  'drow'
+SELECT
+    str,
+    REVERSE (str) AS res
+FROM
+    solution;
