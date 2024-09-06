821b5a7b2fa9dd5c638f856aec32f298ddc42d2b
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2024-09-06 13:01:39 +0200

    Add challenges

---
+++ Typescript Codewars/7kyu/Ch4113ng3.ts
@@ -0,0 +1,9 @@
+/*
+Description
+
+Make your strings more nerdy: Replace all 'a'/'A' with 4, 'e'/'E' with 3 and 'l' with 1 e.g. "Fundamentals" --> "Fund4m3nt41s"
+*/
+
+export function nerdify(txt: string): string {
+  return txt.replace(/[aA]/g, "4").replace(/[eE]/g, "3").replace(/[l]/g, "1");
+}
