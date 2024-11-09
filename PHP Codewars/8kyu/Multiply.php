4817c3b705c4ddd65dfacf91081be52789355712
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2024-11-09 12:09:23 +0100

    Add challenges

---
+++ PHP Codewars/8kyu/Multiply.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Description
+
+This code does not execute properly. Try to figure out why.
+
+function multiply(int $a, int $b): int {
+  $a * $b;
+}
+*/
+  
+function multiply(int $a, int $b): int {
+  return $a * $b;
+}
+?>
---
+++ Python Codewars/8kyu/Multiply.py
@@ -0,0 +1,11 @@
+'''
+Description
+
+This code does not execute properly. Try to figure out why.
+
+def multiply(a, b):
+    a * b
+'''
+
+def multiply(a, b):
+    return a * b
